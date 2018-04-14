29a80773fb3b028cf67b4f860cf2505b781c3273
Alex <wowgss@gmail.com>
2018-04-14 04:38:46 +0300

    Add normal params generator

--- alex/create_dataset.py
+++ alex/create_dataset.py
@@ -34,20 +34,26 @@ def mean_param(port):
 def in_min_max(val, port):
     return port.min_val <= val <= port.max_val
 
-def sens_norm_params(prev, port):
-    s = random.randint(-1, 1)
-    tmp = prev + s
-    if in_min_max(tmp, port):
-        return tmp
-    else:
-        return prev - s
+def sens_norm_params(prev, go_to):
+    tmp = prev + go_to_val(prev, go_to)
+    return tmp
 
-def norm_params(controller, prev):
+def go_to_val(val, go_to):
+    if go_to == val:
+        return 0
+    return (go_to - val) // abs(go_to - val)
+
+def norm_params(controller, prev, go_to):
     dic = {'time': prev['time'] + 1,
-            'sensors': {x : sens_norm_params(prev['sensors'][x], x) for x in controller.ports},
+            'sensors': {x : sens_norm_params(prev['sensors'][x], go_to[x]) for x in controller.ports},
            'mac' : controller.mac
     }
     return dic
+
+def get_A_B(port):
+    delt = (port.max_val - port.min_val) // 7
+    mean_val = (port.min_val + port.max_val) // 2
+    return mean_val - delt, mean_val + delt
 
 def create_norm_params(controller):
     dic = {'time' : time.time(),
@@ -56,12 +62,18 @@ def create_norm_params(controller):
            }
 
     prev = dic
+    ab_vals = {x : get_A_B(x) for x in controller.ports}
+    go_to = {x : random.randint(*ab_vals[x]) for x in ab_vals}
     while True:
-        prev = norm_params(controller, prev)
+        prev = norm_params(controller, prev, go_to)
+        for x in prev['sensors']:
+            if prev['sensors'][x] == go_to[x]:
+                go_to[x] = random.randint(*ab_vals[x])
+
         tmp = {}
         tmp['time'] = prev['time']
         tmp['mac'] = prev['mac']
-        tmp['sensors'] = [(x.id, x.type, prev['sensors'][x]) for x in controller.ports]
+        tmp['sensors'] = [{'id':x.id, 'type':x.type, 'value':prev['sensors'][x]} for x in controller.ports]
         yield tmp
 
 ports = []
@@ -72,10 +84,10 @@ ports.append(Port(4, 7, 30, 100))
 
 contr = Controller(ports, '262g34hg45y4')
 
-for x in create_norm_params(contr):
-    tmp = json.dumps(x, indent=2, sort_keys=True)
-    print(tmp)
-    time.sleep(1)
+# for x in create_norm_params(contr):
+#     tmp = json.dumps(x, indent=2, sort_keys=True)
+#     print(tmp)
+#     time.sleep(1)
 
 
 
